6eb0f270ff51e087e635251c4bd4aea1bf40b5b1
Sohonguy <hoang.nguyen.2202@stud.uni-due.de>
2023-04-25 15:00:32 +0200

    Websiten neu

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { TableComponent } from './pages/table/table.component';
+import { ZweiFaktorComponent } from './pages/zwei-faktor/zwei-faktor.component';
 
 
 
 const routes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'table', component: TableComponent}
+
 
 ];
 
