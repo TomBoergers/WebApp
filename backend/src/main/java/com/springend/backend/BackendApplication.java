5f7e67e1648b92b42e219c430ebd2ad57f63385b
Tom Börgers <tom.boergers@stud.uni-due.de>
2023-04-25 12:13:44 +0200

    Backend NutzerRegistrierung

--- backend/src/main/java/com/springend/backend/BackendApplication.java
+++ backend/src/main/java/com/springend/backend/BackendApplication.java
@@ -1,13 +1,27 @@
 package com.springend.backend;
 
+import com.springend.backend.Nutzer.Nutzer;
+import com.springend.backend.Nutzer.NutzerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(NutzerRepo nutzerRepo) {
+
+        return args -> {
+            nutzerRepo.save(new Nutzer("Test","Name","test@gmail.com", LocalDate.of(1999,1,1),"12345"));
+        };
     }
 
 }
