6f6b32ba422ed2dedb0aec0919f1275fe6b2a440
Tom Börgers <tom.boergers@stud.uni-due.de>
2023-04-27 11:15:52 +0200

    CorsConfig changes

--- backend/src/main/java/com/springend/backend/Nutzer/CorsConfig.java
+++ backend/src/main/java/com/springend/backend/Nutzer/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/*")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("");
             }
