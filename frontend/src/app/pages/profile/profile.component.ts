e1a2258a2db6c733a3c6ec7b93999a87db8f15c5
spsakell <samuel.kelling@stud.uni-due.de>
2023-04-28 20:33:33 +0200

    Profilseite vorläufig + Bilder

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  email: string = '';
+  dateOfBirth: string = '';
+
+  editEmail(): void {
+    const newEmail = prompt('Deine Email:');
+    if (newEmail) {
+      this.email = newEmail;
+    }
+  }
+
+  editDateOfBirth(): void {
+    const newDateOfBirth = prompt('Dein Geburtsdatum');
+    if (newDateOfBirth) {
+      this.dateOfBirth = newDateOfBirth;
+    }
+  }
+
+  url: string|null|ArrayBuffer  ="assets/profile/Profilbild final.png"
+  onFileSelected(files: FileList | null) {
+    if (files) {
+      var reader = new FileReader()
+      reader.readAsDataURL(files[0])
+      reader.onload = (event:Event) => {
+        let fileReader = event.target as FileReader
+        this.url = fileReader.result;
+      }
+    }
+  }
+
+  protected readonly onselect = onselect;
 
 }
