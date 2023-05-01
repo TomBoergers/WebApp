f2039454f4964c070c16431cba80ec369c552c5e
Sohonguy <hoang.nguyen.2202@stud.uni-due.de>
2023-05-01 16:26:07 +0200

    register sysAdmin

---
+++ frontend/src/app/pages/register-admin/register-admin.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { SysAdmin } from 'src/app/classes/sys-admin';
+
+import {error} from "@angular/compiler-cli/src/transformers/util";
+import { RegisteradminService } from 'src/app/services/registeradmin.service';
+
+
+@Component({
+  selector: 'app-register-admin',
+  templateUrl: './register-admin.component.html',
+  styleUrls: ['./register-admin.component.scss']
+})
+export class RegisterAdminComponent {
+  SysAdmin: SysAdmin = new SysAdmin();
+
+  constructor(private registeradminService: RegisteradminService){}
+
+
+  public sysAdminRegister(){
+    this.registeradminService.registerSystemAdmin(this.SysAdmin).subscribe(
+      response => {
+        console.log("Registrierung erfolgreich")
+      },
+      error => {
+        console.log("Registrierung fehlgeschalgen")
+      }
+    );
+
+    ;
+  }
+}
