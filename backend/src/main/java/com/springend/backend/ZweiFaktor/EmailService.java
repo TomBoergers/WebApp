d6da7ec57cfdcb47a556d67fab1e483b2bdd39fa
Janice <janice.chiam@stud.uni-due.de>
2023-05-06 05:27:58 +0200

    code an Email

---
+++ backend/src/main/java/com/springend/backend/ZweiFaktor/EmailService.java
@@ -0,0 +1,27 @@
+package com.springend.backend.ZweiFaktor;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void codeVerschicken(String email, String code) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("your-email@example.com");
+        helper.setTo(email);
+        helper.setSubject("Ihr Verifizierungscode");
+        helper.setText("Ihr Verifizierungscode lautet: " + code);
+
+        mailSender.send(message);
+    }
+}
+
