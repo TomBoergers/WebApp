be63c87efe8f32bc708078efbb633fd60fe6f18a
Sohonguy <hoang.nguyen.2202@stud.uni-due.de>
2023-05-07 14:08:34 +0200

    bugix

--- frontend/src/app/services/auth.guard.ts
+++ frontend/src/app/services/auth.guard.ts
@@ -10,18 +10,18 @@ import {LoginuserService} from "./loginuser.service";
 export class AuthGuard implements CanActivate {
 
 
-  constructor(private loginService: LoginuserService,private roleAs: string) {
+  constructor(private loginService: LoginuserService) {
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (localStorage.getItem("user")){
-      this.roleAs = 'user';
+
       return true;
     }
     else if (localStorage.getItem("admin")){
-      this.roleAs = 'admin';
+
       return true;
     }
 
