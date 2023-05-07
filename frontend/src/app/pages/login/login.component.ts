180ccb206fb0642b430496e44458372229c9d997
Tom Börgers <tom.boergers@stud.uni-due.de>
2023-05-07 14:00:12 +0200

    cleanup

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -2,38 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/classes/user';
 import { LoginuserService } from 'src/app/services/loginuser.service';
 import {Router} from "@angular/router";
-import {BehaviorSubject} from "rxjs";
-import {signUp} from "../../../data-type";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+
   user: User = new User();
 
+  constructor(private loginuserService: LoginuserService, private router: Router){
+  }
 
-
-  constructor(private loginuserService: LoginuserService, private router: Router){}
-ngOnInit() {
+  ngOnInit() {
     this.loginuserService.reloadPage()
-}
+  }
 
   userLogin(data: User){
     console.warn(data);
     this.loginuserService.loginUser(data)
-
-
-
   }
-
-
-
-
-
-
-
-
-
 }
