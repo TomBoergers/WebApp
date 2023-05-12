273b5d0683331c4e53da488988563c8724eaa9e2
Tom Börgers <tom.boergers@stud.uni-due.de>
2023-05-12 18:11:25 +0200

    Docker

--- backend/src/main/java/com/springend/backend/BackendApplication.java
+++ backend/src/main/java/com/springend/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.springend.backend;
 
-import com.springend.backend.Reader.CSVReader.CSVRepo;
 import com.springend.backend.Reader.CSVReader.CSVService;
 import com.springend.backend.Nutzer.Nutzer;
 import com.springend.backend.Nutzer.NutzerRepo;
@@ -27,9 +26,9 @@ public class BackendApplication {
 
         return args -> {
             nutzerRepo.save(new Nutzer("Test","Name","test@gmail.com", LocalDate.of(1999,1,1),"12345"));
-
         };
     }
+
     @Bean
     CommandLineRunner init2(SysAdminRepo sysAdminRepo) {
       return args -> {
@@ -38,17 +37,15 @@ public class BackendApplication {
     }
 
     @Bean
-    CommandLineRunner init3(CSVService csvService, XMLService xmlService){
-
+    CommandLineRunner init3(CSVService csvService, XMLService xmlService) {
         return args -> {
-          csvService.addCSV("backend/src/main/java/com/springend/backend/Datentabellen/aachenvornamen2021-commasep-decimalpoint.csv",4,",","Vornamen der Stadt Aachen","2021");
-          csvService.addCSV("backend/src/main/java/com/springend/backend/Datentabellen/sterbefalle-monatlich-2015_2022.csv", 9, ",", "Sterbefälle", "2015-2022");
-          csvService.addCSV("backend/src/main/java/com/springend/backend/Datentabellen/anzahl-der-arbeitslosen-in-der-stadteregion-aachen22.csv", 5, ";","Anzahl der Arbeitslosen", "2022");
-          csvService.addCSV("backend/src/main/java/com/springend/backend/Datentabellen/anzahl-der-arbeitssuchenden-in-der-stadteregion-aachen22.csv", 5, ";", "Anzahl der Arbeitssuchenden", "2022");
-          csvService.addCSV("backend/src/main/java/com/springend/backend/Datentabellen/geburten-monatlich-2015_2022.csv", 9, ",", "Geburten", "2015-2022");
-          csvService.addCSV("backend/src/main/java/com/springend/backend/Datentabellen/strassennamen.csv",4,",","Strassenliste der Stadt Aachen","2021");
-          xmlService.addXML("backend/src/main/java/com/springend/backend/Datentabellen/Test.xml", "Test1","2000");
-          xmlService.addXML("backend/src/main/java/com/springend/backend/Datentabellen/12411-14iz.xml","Kreisfreiestädte der Stadt Aachen","2000");
+            csvService.addCSV("usr/src/app/Datentabellen/aachenvornamen2021-commasep-decimalpoint.csv", 4, ",", "Vornamen der Stadt Aachen", "2021");
+            csvService.addCSV("usr/src/app/Datentabellen/sterbefalle-monatlich-2015_2022.csv", 9, ",", "Sterbefälle", "2015-2022");
+            csvService.addCSV("usr/src/app/Datentabellen/anzahl-der-arbeitslosen-in-der-stadteregion-aachen22.csv", 5, ";", "Anzahl der Arbeitslosen", "2022");
+            csvService.addCSV("usr/src/app/Datentabellen/anzahl-der-arbeitssuchenden-in-der-stadteregion-aachen22.csv", 5, ";", "Anzahl der Arbeitssuchenden", "2022");
+            csvService.addCSV("usr/src/app/Datentabellen/geburten-monatlich-2015_2022.csv", 9, ",", "Geburten", "2015-2022");
+            csvService.addCSV("usr/src/app/Datentabellen/strassennamen.csv", 4, ",", "Strassenliste der Stadt Aachen", "2021");
+            xmlService.addXML("usr/src/app/Datentabellen/Test.xml", "Test1", "2000");
         };
     }
 }
