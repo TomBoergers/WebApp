c835cfebd6e15d21cb327483e49a82149b5b0ffd
Angelinaleib <angelinaaleib@gmail.com>
2023-06-08 17:36:22 +0200

    Diagram

--- frontend/src/app/pages/diagram/diagram.component.ts
+++ frontend/src/app/pages/diagram/diagram.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, AfterViewInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+
+
 @Component({
   selector: 'diagram',
   templateUrl: './diagram.component.html',
@@ -9,31 +11,70 @@ import {HttpClient} from "@angular/common/http";
 export class DiagramComponent {
 
 
-  constructor(private http: HttpClient) {
+  genderWomen:any = this.getGenderCountWoman();
+  genderMen: any = this.getGenderCountMan();
+  gender: any[][]=  [];
+
+
+  constructor(private httpClient: HttpClient) {
+
   }
 
-  /*ngOnInit() {
-    this.createPieChart();
-  }*/
-
-  ngAfterViewInit() {
+  chartOptions = {
+    animationEnabled: true,
+    title: {
+      text: "Verhältnis männlich und weiblich"
+    },
+    data: [{
+      type: "pie",
+      startAngle: -90,
+      indexLabel: "{name}: {y}",
+      yValueFormatString: "#,###.##'%'",
+      dataPoints: [
+        {y: 30, name: "Women"},
+        {y: 20, name: "Men"}
+      ]
+    }]
   }
 
-  /*createPieChart() {
-    const pieChart = new Chart('pieChart', {
-      type: 'pie',
-      date: {
-        labels: ['Label 1', 'Label 2', 'Label 3'],
-        datasets: [
-          {
-            data: [40, 30, 20],
-            backgroundColor: ['red', 'blue', 'green'],
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-      },
+  ngOnInit() {
+
+  }
+
+
+  getGenderCountWoman() {
+     this.httpClient.get<any[][]>("http://localhost:8080/CSV/1").subscribe(data => {
+       this.gender = data;
+     });
+
+     let women = "w";
+     let countW= 0;
+     for(let j = 0; j < this.gender.length; j++){
+
+           if(this.gender[2][j] === women){
+             countW++;
+           }
+       }
+    return countW;
+     }
+
+
+
+  getGenderCountMan() {
+    this.httpClient.get<any[][]>("http://localhost:8080/CSV/1").subscribe(data => {
+      this.gender = data;
     });
-  }*/
+
+    //let women = "w";
+    let men = "m";
+    //let countW= 0;
+    let countM = 0;
+    for(let i = 0; i < this.gender.length; i++){
+
+          if(this.gender[2][i] === men){
+            countM++;
+          }
+      }
+    return countM;
+    }
 }
