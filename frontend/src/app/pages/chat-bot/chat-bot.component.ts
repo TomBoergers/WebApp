3bf0cd3dd892d7db86370b7d2583d9557129b7cd
Sohonguy <hoang.nguyen.2202@stud.uni-due.de>
2023-06-26 15:54:57 +0200

    joa Chat Bot ohne Antworten

--- frontend/src/app/pages/chat-bot/chat-bot.component.ts
+++ frontend/src/app/pages/chat-bot/chat-bot.component.ts
@@ -14,6 +14,45 @@ export class ChatBotComponent {
   sendMessage(): void {
     if (this.userInput.trim() !== '') {
       this.messages.push(this.userInput);
+        if(this.userInput === 'Hallo'){
+          this.messages.push('Hallo Mensch')
+        }
+        else if(this.userInput === 'Welchen Datenntypen kann mann einlesen?'){
+          this.messages.push('')
+        }
+        else if(this.userInput === 'Wo finde ich mein Profil'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Wo finde ich die Karte?'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Kann ich eigene Dateien einlesen?'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Wo kann ich meine Dateien exportieren?'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Sehen andere User mein Profil?'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Welche Artenn von Diagrammen gibt es?'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Worüber ist diese Website'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Wie erstelle ich einen Post'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Wie finde ich zum Chat Bot'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else if(this.userInput === 'Macht Döner schöner?'){
+          this.messages.push('Hallo, wie geht es dir?')
+        }
+        else {
+          this.messages.push('Darauf habe ich keine antwort')
+        }
       // Perform chatbot logic or API calls here to generate a response
       this.userInput = '';
     }
