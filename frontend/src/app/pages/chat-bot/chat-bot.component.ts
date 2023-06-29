42e60b64eedd2c718cde5c3330a66557e173faaa
Hoang <hoang.nguyen.2202@stud.uni-due.de>
2023-06-29 10:32:24 +0200

    Chatbot Antworten

--- frontend/src/app/pages/chat-bot/chat-bot.component.ts
+++ frontend/src/app/pages/chat-bot/chat-bot.component.ts
@@ -18,37 +18,38 @@ export class ChatBotComponent {
           this.messages.push('Hallo Mensch')
         }
         else if(this.userInput === 'Welchen Datenntypen kann mann einlesen?'){
-          this.messages.push('')
+          this.messages.push('XML, CSV Dateien')
         }
         else if(this.userInput === 'Wo finde ich mein Profil'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('Über die Navigationsleiste über den Reiter Profil.')
         }
         else if(this.userInput === 'Wo finde ich die Karte?'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('Über die Navigationsleiste über den Reiter Karte')
         }
         else if(this.userInput === 'Kann ich eigene Dateien einlesen?'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('Nein.')
         }
         else if(this.userInput === 'Wo kann ich meine Dateien exportieren?'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('In der Tabellen Ansicht über den Button "Export"')
         }
         else if(this.userInput === 'Sehen andere User mein Profil?'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('Ja, über die Freundesliste sehen andere User mein Profil.')
         }
-        else if(this.userInput === 'Welche Artenn von Diagrammen gibt es?'){
-          this.messages.push('Hallo, wie geht es dir?')
+        else if(this.userInput === 'Welche Arten von Diagrammen kann man anzeigen?'){
+          this.messages.push('Man kann die Daten als Balkendiagramm oder als Kreisdiagramm anezigen.')
         }
         else if(this.userInput === 'Worüber ist diese Website'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('Diese Website handelt über die Stadt Aachen.')
         }
         else if(this.userInput === 'Wie erstelle ich einen Post'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('Über den Reiter Forum landest, du auf der Forum Homepage.' +
+            ' Von da aus kannst du auf den Button "Post Create" gehen und dort einen Post erstellen')
         }
-        else if(this.userInput === 'Wie finde ich zum Chat Bot'){
-          this.messages.push('Hallo, wie geht es dir?')
+        else if(this.userInput === 'Kann ich mit meinen Freunden chatten?'){
+          this.messages.push('Ja, du kannst nachdem du deine Freunde hinzugefügt hast mit ihnen chatten.')
         }
         else if(this.userInput === 'Macht Döner schöner?'){
-          this.messages.push('Hallo, wie geht es dir?')
+          this.messages.push('Ja, aufjedenfall.')
         }
         else {
           this.messages.push('Darauf habe ich keine antwort')
